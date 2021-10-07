87ab3dec6b27a64dc7404bd74b1377ef98fde51c
David Zh <david99111@mail.ru>
2021-10-07 21:36:23 +0300

    lab5more

--- lab5/ex00.py
+++ lab5/ex00.py
@@ -3,8 +3,8 @@ from math import *
 import numpy as np
 
 def gen_array(m: int, n: int):
-	rng = np.random.default_rng(180)
-	arr = np.array([(rng.integers(low=0, high=99, size=n)) for i in range(m)])
+	rng = np.random.default_rng(11)
+	arr = np.array([(rng.integers(low=0, high=2, size=n)) for i in range(m)])
 	return arr
 
 def check_triangle(arr: np.ndarray) -> bool:
@@ -30,6 +30,33 @@ def sum_rows(a ,b, weight):
 	a = [temp_a[i] + temp_b[i] for i in range(len(b))]
 	return a
 
+def go_triangle(arr: np.ndarray) -> np.ndarray:
+	try:
+		swaps = 0
+		depth = 0
+		for x in range(n - 1):
+			print(x)
+			main = arr[depth][depth]
+			if (main == 0):
+				toswap = depth + 1
+				while (toswap < n):
+					if (arr[toswap][depth] != 0):
+						arr = swap_rows(arr, depth, toswap)
+						swaps += 1
+						break
+					toswap += 1
+				main = arr[depth][depth]
+			for i in range (depth + 1, n):
+				devisor = -arr[i][depth] / main
+				arr[i] = sum_rows(arr[i], arr[depth], devisor)
+			depth += 1
+		if swaps % 2 == 1:
+			return arr * -1
+		return arr
+	except:
+		print("Данная матрица не может быть преведена к треугольной")
+		return None
+
 try:
 	print("Треугольной может быть только квадратная матрица")
 	#n = int(input("Input N: "))
@@ -41,25 +68,5 @@ except:
 arr = gen_array(n, n)
 print(arr)
 
-try:
-	depth = 0
-	for x in range(n - 1):
-		main = arr[depth][depth]
-		if (main == 0):
-			toswap = depth + 1
-			while (toswap < n):
-				if (arr[toswap][depth] != 0):
-					arr = swap_rows(arr, depth, toswap)
-					break
-				toswap += 1
-			main = arr[depth][depth]
-		for i in range (depth + 1, n):
-			devisor = -arr[i][depth] / main
-			arr[i] = sum_rows(arr[i], arr[depth], devisor)
-		depth += 1
-		print(x)
-		print(arr)
-except:
-	print("Данная матрица не может быть преведена к треугольной")
-print("RES")
+arr = go_triangle(arr)
 print(arr)
