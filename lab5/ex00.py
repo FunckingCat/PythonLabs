ee9d9c80f995deedb915007f963daecdd5a78f47
David Zh <david99111@mail.ru>
2021-10-07 21:51:53 +0300

    triangling_finished

--- lab5/ex00.py
+++ lab5/ex00.py
@@ -3,18 +3,9 @@ from math import *
 import numpy as np
 
 def gen_array(m: int, n: int):
-	rng = np.random.default_rng(11)
+	rng = np.random.default_rng(145)
 	arr = np.array([(rng.integers(low=0, high=2, size=n)) for i in range(m)])
 	return arr
-
-def check_triangle(arr: np.ndarray) -> bool:
-	i = 0
-	for a in arr:
-		for x in range(i):
-			if a[x] != 0:
-				return False
-		i += 1
-	return True
 
 def swap_rows(arr, a, b):
 	arr = list(arr)
@@ -30,12 +21,13 @@ def sum_rows(a ,b, weight):
 	a = [temp_a[i] + temp_b[i] for i in range(len(b))]
 	return a
 
-def go_triangle(arr: np.ndarray) -> np.ndarray:
+def go_triangle(arr_src: np.ndarray, n) -> np.ndarray:
+	arr = np.eye(n)
+	np.copyto(arr, arr_src)
 	try:
 		swaps = 0
 		depth = 0
 		for x in range(n - 1):
-			print(x)
 			main = arr[depth][depth]
 			if (main == 0):
 				toswap = depth + 1
@@ -50,6 +42,9 @@ def go_triangle(arr: np.ndarray) -> np.ndarray:
 				devisor = -arr[i][depth] / main
 				arr[i] = sum_rows(arr[i], arr[depth], devisor)
 			depth += 1
+		if arr[n-1][n-1] == 0:
+			print("Данная матрица не может быть преведена к треугольной")
+			return None
 		if swaps % 2 == 1:
 			return arr * -1
 		return arr
@@ -60,13 +55,13 @@ def go_triangle(arr: np.ndarray) -> np.ndarray:
 try:
 	print("Треугольной может быть только квадратная матрица")
 	#n = int(input("Input N: "))
-	n = 4
+	n = 6
 except:
 	print("Value Error")
 	exit()
 
 arr = gen_array(n, n)
-print(arr)
+arr_tri = go_triangle(arr, n)
 
-arr = go_triangle(arr)
 print(arr)
+print(arr_tri)
